211affaa907a467ed444306e86519ab09df0737b
ikram560 <ldikramuddin@gmail.com>
2024-05-21 19:53:42 +0800

    Add files via upload

---
+++ praktikum1/src/nomor4/belanja.java
@@ -0,0 +1,21 @@
+
+package nomor4;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class belanja {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> mahasiswa = new ArrayList<>();
+        
+        int pilihan;
+        
+        do{
+            System.out.println("daftar mahasiswa :");
+            
+        }
+        
+        
+        
+    }
+}
